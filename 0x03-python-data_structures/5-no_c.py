d94bc28f8b01bcf86b6e7f9fd1bee101d4714182
Lungastafa2 <lungastafa@gmail.com>
2023-05-15 14:35:36 -0700

    nu5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    my_string = my_string.translate({ord(i): None for i in 'Cc'})
-    return my_string
+    my_string2 = ""
+    for i in range(len(my_string)):
+        if (my_string[i] == "c"):
+            continue
+        elif (my_string[i] == "C"):
+            continue
+        else:
+            my_string2 += my_string[i]
+    return my_string2
